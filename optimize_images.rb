77fc0d9b46477c75ad896b57b800a0899c854487
Julian Nadeau <julian@jnadeau.ca>
2025-06-29 19:16:11 -0400

    Output optimization

--- optimize_images.rb
+++ optimize_images.rb
@@ -5,6 +5,7 @@ base_dir = __dir__
 Dir.chdir(base_dir) do
   Dir.glob("assets/img/slideshow/*.{jpg,png,jpeg}").each do |image_path|
     logger.info("Optimizing image: #{image_path}")
+    original_file_size = File.size(image_path)
 
     # Remove all metadata from the image
     logger.info("Removing metadata from image: #{image_path}")
@@ -27,5 +28,11 @@ Dir.chdir(base_dir) do
     # Optimize the image using ImageMagick
     logger.info("Optimizing image: #{image_path} with flags: #{extra_flags}")
     system("magick '#{image_path}' -strip -quality 85 #{extra_flags} '#{image_path}'")
+
+    # Check if the optimization was successful
+    file_size = File.size(image_path)
+    reduction_bytes = original_file_size - file_size
+    reduction_kb = reduction_bytes / 1024.0
+    puts "Optimized image size: #{file_size} bytes, original size: #{original_file_size} bytes. Reduced by #{reduction_kb} KB."
   end
 end
